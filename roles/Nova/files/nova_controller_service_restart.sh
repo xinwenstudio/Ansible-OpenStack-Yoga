a9bfaf4b6be9931c721b15ed81b491b0f49cbdb8
LRAltas <88080680+lrAltas@users.noreply.github.com>
2025-08-25 21:06:19 +0800

    Delete roles directory

--- roles/Nova/files/nova_controller_service_restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 处理api服务
-systemctl restart openstack-nova-api
-# 处理资源调度服务
-systemctl restart openstack-nova-scheduler
-# 处理数据库服务
-systemctl restart openstack-nova-conductor
-# 处理vnc远程窗口服务
-systemctl restart openstack-nova-novncproxy
